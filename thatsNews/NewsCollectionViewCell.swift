904963005c340cf23561e5df71531909a88d14dd
Ferin.Chow <nrrri.cho@gmail.com>
2024-07-20 22:36:52 -0400

    catched up lesson 6

---
+++ thatsNews/NewsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  NewsCollectionViewCell.swift
+//  thatsNews
+//
+//  Created by Narissorn Chowarun on 2024-07-20.
+//
+
+import UIKit
+
+class NewsCollectionViewCell: UICollectionViewCell {
+    
+}
--- thatsNews/NewsCollectionViewController.swift
+++ thatsNews/NewsCollectionViewController.swift
@@ -3,11 +3,11 @@
 //  thatsNews
 //
 //  Created by Narissorn Chowarun on 2024-06-24.
-//
+// 7fc04c9461f344a88df679e902356464
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsCollectionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
